089935dedfa69acd2c89d6e91d789e629923d524
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-06-08 20:11:44 -0600

    It can now sort reverse lists

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function mergeSort(arr) {
     let secondHalf = arr.slice(arr.length / 2, arr.length + 1);
 
     if (arr.length < 2) {
-        return
+        return arr;
     }
     else if (arr.length === 2) {
         if (firstHalf[0] > secondHalf[0]){
@@ -23,11 +23,14 @@ function mergeSort(arr) {
         }
     }
     else if (arr.length > 2) {
-        
+        //console.log(firstHalf);
+        //console.log(secondHalf);
+        return [...mergeSort(secondHalf), ...mergeSort(firstHalf)];
     }
 
 }
 
-console.log(mergeSort([3, 2, 1]));
+console.log(mergeSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 console.log(mergeSort([1,2]));
 console.log(mergeSort([2,1]));
+console.log(mergeSort([5, 8, 2, 9, 6, 3, 7, 1, 10, 4]));
