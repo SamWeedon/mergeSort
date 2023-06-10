b3465ecd5dcfd8470d72ee783be850b9edde49a1
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-06-09 22:26:06 -0600

    Finished project

--- script.js
+++ script.js
@@ -7,30 +7,33 @@ function mergeSort(arr) {
         sort left half of elements
         sort right half of elements
         merge sorted halves
-    */
+*/
     let firstHalf = arr.slice(0, arr.length / 2);
-    let secondHalf = arr.slice(arr.length / 2, arr.length + 1);
+    let secondHalf = arr.slice(arr.length / 2);
 
     if (arr.length < 2) {
         return arr;
     }
-    else if (arr.length === 2) {
-        if (firstHalf[0] > secondHalf[0]){
-            return mergeSort([...secondHalf, ...firstHalf]);
-        }
-        else if (firstHalf[0] < secondHalf[0]){
-            return [...firstHalf, ...secondHalf];
-        }
+    else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf));
     }
-    else if (arr.length > 2) {
-        //console.log(firstHalf);
-        //console.log(secondHalf);
-        return [...mergeSort(secondHalf), ...mergeSort(firstHalf)];
-    }
-
 }
 
-console.log(mergeSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
-console.log(mergeSort([1,2]));
-console.log(mergeSort([2,1]));
-console.log(mergeSort([5, 8, 2, 9, 6, 3, 7, 1, 10, 4]));
+function merge(left, right) {
+    let mergedArr = [];
+    while (left.length && right.length) {
+        if (left[0] > right[0]) {
+            mergedArr.push(right.shift());
+        }
+        else if (right[0] > left[0]) {
+            mergedArr.push(left.shift());
+        }
+    }
+    return [...mergedArr, ...left, ...right];
+}
+//console.log(mergeSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+//console.log(mergeSort([2,1]));
+//console.log(mergeSort([3,2,1]));
+//console.log(mergeSort([5, 8, 2, 9, 6, 3, 7, 1, 10, 4]));
+//console.log(mergeSort([4,3,1,2]));
+//console.log(mergeSort([3,1,2,4]));
